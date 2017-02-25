17af6f83bb9a4e88ba5f747cd478432e3d10034b
getveloce <office@getveloce.com>
2017-02-25 15:26:58 +0100

    update #15

--- includes/update.php
+++ includes/update.php
@@ -9,7 +9,7 @@ function DisplayUpdate(){
   <div class="row">
   <div class="col-lg-12">
   <div class="panel panel-primary">
-  <div class="panel-heading"><i class="fa fa-cube fa-fw"></i> Update</div>
+  <div class="panel-heading"><i class="fa fa-exchange fa-fw"></i> Update</div>
   <div class="panel-body">
 
     <div class="row">
@@ -17,11 +17,15 @@ function DisplayUpdate(){
     <div class="panel panel-default">
     <div class="panel-body">
       <h4>Release Information</h4>
-      <div class="info-item">Revision</div> v 1.0.1.</br>
+      <div class="info-item">Revision</div> v 1.0.1</br>
     </div><!-- /.panel-body -->
     </div><!-- /.panel-default -->
     </div><!-- /.col-md-6 -->
     </div><!-- /.row -->
+
+    <form action="?page=system_info" method="POST">
+      <input type="button" class="btn btn-outline btn-primary" value="Refresh" onclick="document.location.reload(true)" />
+    </form>
 
   </div><!-- /.panel-body -->
   </div><!-- /.panel-primary -->
