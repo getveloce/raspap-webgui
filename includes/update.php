8e6dac919ea2a5d06ae99cd183022fea7f21073c
getveloce <office@getveloce.com>
2017-02-25 16:00:46 +0100

    update #25

--- includes/update.php
+++ includes/update.php
@@ -31,7 +31,7 @@ function DisplayUpdate(){
     </div><!-- /.panel-default -->
     </div><!-- /.col-md-6 -->
     </div><!-- /.row -->
-    <div class="info-item"><?php echo $data_update_info["url"]; echo " blubb "; var_dump($data_update_info); ?></div></br>
+    <div class="info-item"><?php echo $data_update_info["url"]; echo " blubb "; var_dump($data_update_info); echo dirname(__FILE__); ?></div></br>
     <form action="?page=update_info" method="POST">
       <input type="submit" class="btn btn-warning" name="check_update" value="Check for Updates" />
       <input type="button" class="btn btn-outline btn-primary" value="Refresh" onclick="document.location.reload(true)" />
