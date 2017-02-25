94403dd494c3ab2cd1f013e4af3650702e4a30ea
getveloce <office@getveloce.com>
2017-02-25 19:55:33 +0100

    update #50

--- includes/update.php
+++ includes/update.php
@@ -47,11 +47,8 @@ function DisplayUpdate(){
       $update_output = array();
       $update_return_var;
       exec("sudo /var/sudowebscript.sh rm_html_dir", $update_output, $update_return_var);
-
-      echo $update_return_var;
-      echo "<br />";
-      var_dump($update_output);
-      echo "<br />";
+      exec("sudo /var/sudowebscript.sh clone_wifi_portal", $update_output, $update_return_var);
+      exec("sudo /var/sudowebscript.sh chown_html_dir", $update_output, $update_return_var);
 
       /*exec("sudo git clone https://github.com/getveloce/raspap-webgui /var/www/html", $update_output, $update_return_var);
 
