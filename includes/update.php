39b828dcaf2c92de84dc93a50e2d4e23a0b1bceb
getveloce <office@getveloce.com>
2017-02-25 15:22:51 +0100

    update #13

--- includes/update.php
+++ includes/update.php
@@ -9,8 +9,19 @@ function DisplayUpdate(){
   <div class="row">
   <div class="col-lg-12">
   <div class="panel panel-primary">
-  <div class="panel-heading"><i class="fa fa-cube fa-fw"></i> System</div>
+  <div class="panel-heading"><i class="fa fa-cube fa-fw"></i> Update</div>
   <div class="panel-body">
+
+    <div class="row">
+    <div class="col-md-6">
+    <div class="panel panel-default">
+    <div class="panel-body">
+      <h4>Release Information</h4>
+      <div class="info-item">Revision</div> v 1.0.1.</br>
+    </div><!-- /.panel-body -->
+    </div><!-- /.panel-default -->
+    </div><!-- /.col-md-6 -->
+    </div><!-- /.row -->
 
     <form action="?page=system_info" method="POST">
       <input type="submit" class="btn btn-warning" name="system_reboot"   value="Reboot" />
