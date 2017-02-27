e6b9e42a0bf8464894708725a7f0e544b0f652d2
getveloce <office@getveloce.com>
2017-02-27 09:45:52 +0100

    update #66

--- includes/update.php
+++ includes/update.php
@@ -51,7 +51,7 @@ function DisplayUpdate(){
       echo $update_return_var;
       echo $last_line;*/
 
-      $cmd = "sudo /var/sudowebscript.sh update_wifi_portal " . $data_update_info["wifi_portal_url"];
+      $cmd = "ping 127.0.0.1";
 
       $descriptorspec = array(
          0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
