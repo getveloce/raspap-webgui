6485a7c39d05fc1d0dd4bdc141f23cb1741bd5ca
getveloce <office@getveloce.com>
2017-02-27 13:08:02 +0100

    update #116

--- includes/update.php
+++ includes/update.php
@@ -1,5 +1,47 @@
 <?php
 
+function write_ini_file($assoc_arr, $path, $has_sections=FALSE) {
+    $content = "";
+    if ($has_sections) {
+        foreach ($assoc_arr as $key=>$elem) {
+            $content .= "[".$key."]\n";
+            foreach ($elem as $key2=>$elem2) {
+                if(is_array($elem2))
+                {
+                    for($i=0;$i<count($elem2);$i++)
+                    {
+                        $content .= $key2."[] = \"".$elem2[$i]."\"\n";
+                    }
+                }
+                else if($elem2=="") $content .= $key2." = \n";
+                else $content .= $key2." = \"".$elem2."\"\n";
+            }
+        }
+    }
+    else {
+        foreach ($assoc_arr as $key=>$elem) {
+            if(is_array($elem))
+            {
+                for($i=0;$i<count($elem);$i++)
+                {
+                    $content .= $key."[] = \"".$elem[$i]."\"\n";
+                }
+            }
+            else if($elem=="") $content .= $key." = \n";
+            else $content .= $key." = \"".$elem."\"\n";
+        }
+    }
+
+    if (!$handle = fopen($path, 'w')) {
+        return false;
+    }
+
+    $success = fwrite($handle, $content);
+    fclose($handle);
+
+    return $success;
+}
+
 function DisplayUpdate(){
 
 ?>
@@ -100,6 +142,14 @@ function DisplayUpdate(){
     echo '  <form action="?page=update_info" method="POST">
               <input type="submit" class="btn btn-warning" name="OK" value="OK" />
             </form>';
+
+    $new_revision_data = array(
+                    "revision" => array(
+                        "wifi_portal_revision" => $data_update_info["wifi_portal_revision"],
+                        "workspace_revision" => $data_update_info["workspace_revision"],
+                        "jsps_revision" => $data_update_info["jsps_revision"],
+                    ));
+    write_ini_file($new_revision_data, "update_info.ini", TRUE);
   }
    ?>
 
