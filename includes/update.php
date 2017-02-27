bbd7ec139d0cbba9eadd519be5a7ff1dfb7797ad
getveloce <office@getveloce.com>
2017-02-27 12:28:54 +0100

    update #107

--- includes/update.php
+++ includes/update.php
@@ -84,6 +84,7 @@ function DisplayUpdate(){
     ob_flush();
     $process = proc_open($cmd, $descriptorspec, $pipes, realpath('./'), array());
     echo "<pre>";
+    echo $cmd;
     if (is_resource($process)) {
         while ($s = fgets($pipes[1])) {
             print $s;
