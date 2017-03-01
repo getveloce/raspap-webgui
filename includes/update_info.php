382cf843be202afa03da543aaa82e3d232eddb51
getveloce <office@getveloce.com>
2017-03-01 20:49:12 +0100

    update #130

--- includes/update_info.php
+++ includes/update_info.php
@@ -5,7 +5,7 @@
     "wifi_portal_url" => "https://github.com/getveloce/raspap-webgui.git",
     "workspace_revision" => "1.0.1",
     "workspace_url" => "example.com",
-    "jsps_revision" => "1.88",
+    "jsps_revision" => "1.91",
     "jsps_url" => "example.com"
   );
 
